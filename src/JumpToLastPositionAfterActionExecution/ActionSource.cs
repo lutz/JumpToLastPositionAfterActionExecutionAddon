37277ff862dd77233aaaad13999dc8c26b9fd9d4
Daniel Lutz <daniel.lutz@outlook.com>
2019-11-27 19:38:00 +0100

    code init

---
+++ src/JumpToLastPositionAfterActionExecution/ActionSource.cs
@@ -0,0 +1,21 @@
+﻿using pdftron.PDF;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Pdf;
+
+namespace JumpToLastPositionAfterActionExecution
+{
+    public class ActionSource
+    {
+        public ActionSource(MainForm form, PDFViewWPF viewer)
+        {
+            Form = form;
+            Viewer = viewer;
+        }
+
+        public MainForm Form { get; }
+
+        public PDFViewWPF Viewer { get; }
+
+        public EntryPoint EntryPoint { get; set; }
+    }
+}
---
+++ src/JumpToLastPositionAfterActionExecution/ActionSources.cs
@@ -0,0 +1,38 @@
+﻿using pdftron.PDF;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Pdf;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JumpToLastPositionAfterActionExecution
+{
+    public class ActionSources : List<ActionSource>
+    {
+        public EntryPoint GetSourceEntryPoint(MainForm form)
+        {
+            return this.FirstOrDefault(obj => obj.Form.Equals(form))?.EntryPoint;
+        }
+
+        public ActionSource GetActionSource(MainForm mainForm)
+        {
+            return this.FirstOrDefault(obj => obj.Form.Equals(mainForm));
+        }
+
+        public ActionSource GetActionSource(PDFViewWPF viewer)
+        {
+            return this.FirstOrDefault(obj => obj.Viewer.Equals(viewer));
+        }
+
+        public void RemoveAt(MainForm form)
+        {
+            var obj = GetActionSource(form);
+            if (obj != null) Remove(obj);
+        }
+
+        public void ResetEntryPoint(MainForm form)
+        {
+            var obj = GetActionSource(form);
+            if (obj != null) obj.EntryPoint = null;
+        }
+    }
+}
